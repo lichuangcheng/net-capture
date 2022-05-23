63805c610af87a91b817e52a6ac936996b3eb16f
lichuangcheng <lichuangcheng@zeewain.com>
2022-05-23 18:03:59 +0800

    fix(udp_view): 修复playload计算错误的问题

--- include/netcap/udp_packet_view.h
+++ include/netcap/udp_packet_view.h
@@ -3,7 +3,6 @@
 
 #include <span>
 #include <netcap/exception.h>
-#include <netcap/addr_ipv4.h>
 #include <netcap/byte.h>
 
 class UdpPacketView {
@@ -27,19 +26,19 @@ public:
         return as_host<uint16_t>(&d[2]);
     }
 
-    // UDP 数据报长度，包含报文头和数据长度, 所以最小为 8。
-    uint8_t lenght() const noexcept {
+    // UDP 数据报长度，包含报文头和数据长度, 所以最小为 8
+    uint16_t lenght() const noexcept {
         return as_host<uint16_t>(&d[4]);
     }
 
-    // 校验和 对首部和数据两部分执行 CRC 算法，检验报文是否损坏
+    // 校验和
     uint16_t checksum() const noexcept {
         return as_host<uint16_t>(&d[6]);
     }
 
-    // TCP负载数据
+    // UDP负载数据
     std::span<uint8_t> playload() const noexcept {
-        return d.subspan(lenght() - HEADER_LENGTH);
+        return d.subspan(HEADER_LENGTH, lenght() - HEADER_LENGTH);
     }
 
 private:
